b457a11dd17a96a10fd0a2052f80d63ac10a8d20
NewBavli <vlad.babich.97@inbox.ru>
2021-03-14 18:36:27 +0300

    feat: change code "prototype"

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = function towelSort (matrix) {
     if (!matrix || !matrix.length) return [];
     var sortMatrix =[];
     for (let i=0; i<matrix.length; i++){
-        if (i%2==0) Array.prototype.push.apply(sortMatrix,matrix[i]) 
-        else Array.prototype.push.apply(sortMatrix,matrix[i].reverse());
+        if (i%2==0) sortMatrix.push.apply(sortMatrix,matrix[i]) 
+        else sortMatrix.push.apply(sortMatrix,matrix[i].reverse());
     }
     return sortMatrix;
 }
