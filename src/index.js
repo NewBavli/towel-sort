7e68f22a2a71f423a5926a8101d997f208544adc
NewBavli <vlad.babich.97@inbox.ru>
2021-03-14 17:18:18 +0300

    init: add code to test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || !matrix.length) return [];
+    var sortMatrix =[];
+    for (let i=0; i<matrix.length; i++){
+        if (i%2==0) Array.prototype.push.apply(sortMatrix,matrix[i]) 
+        else Array.prototype.push.apply(sortMatrix,matrix[i].reverse());
+    }
+    return sortMatrix;
 }
